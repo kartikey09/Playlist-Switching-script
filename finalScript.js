7babd58fd02c788b9e7c6162eb2831cf45435c6a
kartikey09 <44106280+kartikey09@users.noreply.github.com>
2021-04-11 10:03:18 +0530

    Add files via upload

---
+++ finalScript.js
@@ -0,0 +1,117 @@
+const pup = require("puppeteer");
+const fs = require("fs");
+
+async function copyYTMdata()
+{
+    let browser = await pup.launch({
+        defaultViewport : false,
+        headless : false,
+        args: ["--start-fullscreen"]
+    });
+
+    const page = await browser.newPage();
+    await page.goto("https://music.youtube.com/playlist?list=RDCLAK5uy_nbTnrBv4CxZys35IAzhO0-fFCiKD58qzo");
+    await autoScroll(page);
+    await page.waitForSelector(".title.style-scope.ytmusic-responsive-list-item-renderer.complex-string a");
+    let allMusic = await page.$$(".title.style-scope.ytmusic-responsive-list-item-renderer.complex-string a");
+    let ytPlaylist = [];
+    for(i = 0; i< allMusic.length; i++)
+    {  
+          ytPlaylist.push({ "song" : await page.evaluate(function(ele){
+            return ele.textContent;
+        }, allMusic[i])});
+    }
+
+    fs.writeFileSync("songNames.js", JSON.stringify(ytPlaylist));
+    browser.close();
+    await pasteOnSpotify();
+}
+
+async function autoScroll(page){
+
+
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
+
+async function pasteOnSpotify(){
+
+    let id ="mikex88617@684hh.com";
+    let pass = "temp123*";
+    let browser = await pup.launch({
+        defaultViewport : false,
+        headless : false,
+        args: ["--start-fullscreen"]
+        
+    });
+
+    const page = await browser.newPage();
+    await page.goto("https://open.spotify.com/");
+    await page.waitForSelector("button[data-testid='login-button']");
+    await page.click("button[data-testid='login-button']")
+    await page.waitForSelector("#login-username");
+    await page.type("#login-username", id);
+    await page.type("#login-password", pass);
+    await page.click("#login-button");
+    await page.waitForSelector("button[data-testid='create-playlist-button']");
+    await new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve();
+        }, 1000);
+    })
+    await page.click("button[data-testid='create-playlist-button']");
+    await page.waitForSelector("._655bc45ccbf3d91c685865ff470892eb-scss.f3fc214b257ae2f1d43d4c594a94497f-scss");
+
+
+    //SEARCHING THE SCRAPPED DATA
+    let data = fs.readFileSync("./songNames.js", "utf-8");
+    data = JSON.parse(data);
+    for(let i = 0; i< data.length; i++)
+        data[i].song = data[i].song.replace("feat.", "");
+
+    
+    for(let i= 0; i< data.length; i++)
+    {
+        try{
+            await page.click("._655bc45ccbf3d91c685865ff470892eb-scss.f3fc214b257ae2f1d43d4c594a94497f-scss");//INPUT BAR
+            await page.type("._655bc45ccbf3d91c685865ff470892eb-scss.f3fc214b257ae2f1d43d4c594a94497f-scss", data[i].song.replace(/\([^()]*\)/g, ''));
+            await new Promise((resolve)=>{
+                return setTimeout(()=>{
+                    resolve();
+               }, 1000);
+            });
+            await page.click(".b490086127ec0ecdc7b170c03de9c5b1-scss");//ADD BUTTON
+            await page.click("path[stroke='currentColor']");//CANCEL BUTTON
+            await page.click("._655bc45ccbf3d91c685865ff470892eb-scss.f3fc214b257ae2f1d43d4c594a94497f-scss");//INPUT BAR
+            new Promise((resolve)=>{
+               return setTimeout(()=>{
+                   resolve();
+                }, 150);
+            });   
+        }
+        catch{
+            await page.click("path[stroke='currentColor']");//CANCEL BUTTON
+            await page.click("._655bc45ccbf3d91c685865ff470892eb-scss.f3fc214b257ae2f1d43d4c594a94497f-scss");//INPUT BAR
+            continue;
+        }
+    }
+
+    browser.close();
+}
+copyYTMdata();
+
